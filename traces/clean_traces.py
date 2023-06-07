012844e4674b964e7d4ed1a09b517a96afc7c6e0
Justin Dong <justindong@u.northwestern.edu>
2023-06-07 00:15:39 -0500

    Fixed bug

--- traces/clean_traces.py
+++ traces/clean_traces.py
@@ -1,14 +1,19 @@
-from datatime import timedelta
-def clean_traces(filename):
+import sys
+
+input_file = sys.argv[1] + ".log"
+output_file = sys.argv[1] + ".parsed"
+
+def clean_traces(input_file, output_file):
     data = []
     line_number = 0
     start_time = 0
     unique_irqs = set()
     num_irqs = 0
     num_interrupts = 0
-    with open(filename, "r") as file:
+    output_string = ""
+    with open(input_file, "r") as file:
         entry_time = ""
-        for line in file:``
+        for line in file:
             line_number += 1
             line = line.strip()
             # print(line_number)
@@ -23,11 +28,21 @@ def clean_traces(filename):
                     irq_number = line.split("vector=")[1].split()[0]
                 else:
                     irq_number = line.split("irq=")[1].split()[0]
-                interrupt_length = float(line.split()[2].split(":")[0]) * 1e9 - entry_time
+                exit_time = float(line.split()[2].split(":")[0]) * 1e9 - start_time
+                interrupt_length = exit_time - entry_time
                 event = line.split()[3].rstrip(":")
-                data.append("IRQ: {}, Entry: {}, Length: {}".format(irq_number, entry_time, interrupt_length)
+                curr_string = "IRQ: {}, Entry: {}, Length: {}".format(irq_number, entry_time, interrupt_length)
+                data.append(curr_string)
+                output_string += curr_string + "\n"
                 unique_irqs.add(irq_number)
     unique_irqs = list(unique_irqs)
     num_irqs = len(unique_irqs)
     num_interrupts = len(data)
+
+    with open(output_file, 'w') as file_write:
+                    file_write.write(output_string + "\n")
+
     return num_irqs, unique_irqs, num_interrupts, data
+
+
+clean_traces(input_file, output_file)
